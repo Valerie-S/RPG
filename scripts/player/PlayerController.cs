4a4cad30b182525760b8df7e684b3f99f5af110f
Val <tianhuiliu17@gmail.com>
2019-01-24 12:59:37 -0500

    Add files via upload

---
+++ scripts/player/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D PlayerBody;
+    private Animator PlayerAnim;
+    private bool isMoving;
+    private Vector2 lastMove;
+
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+        PlayerBody = GetComponent<Rigidbody2D>();
+        PlayerAnim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        isMoving = false;
+        if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            isMoving = true;
+            lastMove.x = Input.GetAxisRaw("Horizontal");
+            PlayerBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed,PlayerBody.velocity.y);
+        }
+        if(Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+        {
+            PlayerBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), PlayerBody.velocity.y);
+        }
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            isMoving = true;
+            lastMove.y = Input.GetAxisRaw("Vertical");
+            PlayerBody.velocity = new Vector2(PlayerBody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+        }
+        if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+        {
+            PlayerBody.velocity = new Vector2(PlayerBody.velocity.x, Input.GetAxisRaw("Vertical"));
+        }
+        PlayerAnim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        PlayerAnim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        PlayerAnim.SetFloat("lastMoveX", lastMove.x);
+        PlayerAnim.SetFloat("lastMoveY", lastMove.y);
+        PlayerAnim.SetBool("isMoving", isMoving);
+ 
+    }
+}
---
+++ tileset/roguelikeSheet_transparent.tsx
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="roguelikeSheet_transparent" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
+ <image source="C:/Users/54189/Desktop/New folder (5)/New folder/Spritesheet/roguelikeSheet_transparent.png" width="968" height="526"/>
+ <terraintypes>
+  <terrain name="Dirt" tile="520"/>
+ </terraintypes>
+ <tile id="518" terrain="0,0,0,"/>
+ <tile id="519" terrain="0,0,,0"/>
+ <tile id="520" terrain=",,,0"/>
+ <tile id="521" terrain=",,0,0"/>
+ <tile id="522" terrain=",,0,"/>
+ <tile id="537">
+  <objectgroup draworder="index">
+   <object id="1" x="0.181818" y="-0.0909091" width="15.8182" height="16.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="575" terrain="0,,0,0"/>
+ <tile id="576" terrain=",0,0,0"/>
+ <tile id="577" terrain=",0,,0"/>
+ <tile id="578" terrain="0,0,0,0"/>
+ <tile id="579" terrain="0,,0,"/>
+ <tile id="583">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.0909091" y="0" width="16" height="15.9091"/>
+  </objectgroup>
+ </tile>
+ <tile id="586">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0909091" y="-0.181818" width="15.9091" height="16.2727"/>
+  </objectgroup>
+ </tile>
+ <tile id="593">
+  <objectgroup draworder="index">
+   <object id="1" x="0.181818" y="0.0909091" width="15.8182" height="15.9091"/>
+  </objectgroup>
+ </tile>
+ <tile id="616">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.0909091" y="-0.0909091" width="15.9091" height="16.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="617">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.0909091" width="16.1818" height="16.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="618">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.272727" width="16.0909" height="16.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="619">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0909091" y="-0.0909091" width="15.9091" height="15.9091"/>
+  </objectgroup>
+ </tile>
+ <tile id="634" terrain=",0,,"/>
+ <tile id="635" terrain="0,0,,"/>
+ <tile id="636" terrain="0,,,"/>
+ <tile id="640">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.181818" width="15.9091" height="16.1818"/>
+  </objectgroup>
+ </tile>
+ <tile id="643">
+  <objectgroup draworder="index">
+   <object id="1" x="0.181818" y="-0.0909091" width="15.7273" height="15.8182"/>
+   <object id="2" x="0.272727" y="-0.181818" width="15.6364" height="15.9091"/>
+  </objectgroup>
+ </tile>
+ <tile id="650">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.0909091" width="16.0909" height="16.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="673">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0909091" y="0" width="15.8182" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="674">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.181818" y="-0.272727" width="16.1818" height="16.4545"/>
+  </objectgroup>
+ </tile>
+ <tile id="675">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.272727" width="16.0909" height="16.3636"/>
+  </objectgroup>
+ </tile>
+ <tile id="676">
+  <objectgroup draworder="index">
+   <object id="1" x="0.181818" y="-0.181818" width="15.8182" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
